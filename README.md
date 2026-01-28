68d7ab1c6cb99196047e6bc6a9600b612a99e33c
MdRehanAli <rehanali21d@gmail.com>
2026-01-28 18:00:17 +0600

    Added readme file

---
+++ README.md
@@ -0,0 +1,25 @@
+# Dashboard
+
+A modern dashboard application built with React, Vite, Tailwind CSS, and DaisyUI.
+
+1. **Clone Repository**
+   ```terminal
+   git clone https://github.com/MdRehanAli/dashboard.git
+   ```
+
+
+2. **Install dependencies**
+   ```terminal
+   npm install
+   ```
+
+3. **Start the development server**
+   ```terminal
+   npm run dev
+   ```
+
+4. **Open your browser**
+   Navigate to `http://localhost:5173` (or the port shown in your terminal)
+
+## 🔗 Live Link:
+[👉 Visit Dashboard Website](https://dashboard-21.vercel.app/)
--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -15,6 +15,7 @@ const DashboardLayout = () => {
         "/appointments": "Appointments",
         "/settings": "Settings"
     }
+
     const DynamicTitle = title[location.pathname];
 
     return (
@@ -22,12 +23,12 @@ const DashboardLayout = () => {
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 <nav className="navbar w-full bg-accent">
-                    <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                    <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost hover:bg-info hover:border-none">
                         {/* Sidebar toggle icon */}
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className=" inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
                     </label>
                     <div className='flex justify-between items-center w-full'>
-                        <div className="px-4">{DynamicTitle}</div>
+                        <div className="px-4 text-[32px]">{DynamicTitle}</div>
                         <div className='flex items-center gap-10'>
                             <div className=''><IoMdNotificationsOutline className='text-[32px]' />
                             </div>
